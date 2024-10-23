9ee5ef5d036354df155905e4c3eb2a13819c0ece
Christian Bernal <bernal.johnchristian.08242003@gmail.com>
2024-10-23 17:51:17 +0800

    yaml modified for assets directory

--- lib/bob_the_rodent.dart
+++ lib/bob_the_rodent.dart
@@ -1,3 +1,23 @@
+import 'dart:async';
+
+import 'package:bob_the_rodent/levels/level.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
-class BobTheRodent extends FlameGame {}
+class BobTheRodent extends FlameGame {
+  late final CameraComponent cam;
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+      world: world,
+      width: 640,
+      height: 360,
+    );
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
