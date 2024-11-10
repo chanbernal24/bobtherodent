8278adb6a9ead9fb30830b199ffebdac45186d54
Miguel Catalan <catalanjuanmiguel@gmail.com>
2024-11-10 17:44:05 +0800

    tiles

---
+++ assets/tiles/Forest1/River (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="River (1)" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <image source="C:/Users/Katrina/Downloads/River (1).jpg" width="1920" height="1080"/>
+</tileset>
