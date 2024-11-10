b1fe7b4ae4f8f730bd9aa753ff91c89edfd26cc0
Russell Barro <Russellabarro@gmail.com>
2024-11-10 17:05:18 +0800

    initial commit

--- assets/tiles/Bob the Rodent.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Bob the Rodent" tilewidth="16" tileheight="16" tilecount="242" columns="22">
- <image source="../images/terrain/tiles.png" width="352" height="176"/>
-</tileset>
